4f47bb507a88461de380283402b2e3332ca9626f
EzechielKiregha <ekiregha@gmail.com>
2023-03-17 15:16:02 -0700

    Printing digits using the ASCII Code

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -We print all digit to the std output.
+ * using for loop through 0 - 10 print via putchar
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
