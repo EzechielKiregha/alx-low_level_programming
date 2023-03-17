e069ce4b8aaaaf97f692364fccc2c70b45d72a5f
EzechielKiregha <ekiregha@gmail.com>
2023-03-17 16:02:27 -0700

    All possible combinations of a single-digit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -We print all digit to the std output.
+ * using for loop through 0 - 10 print via putchar
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
