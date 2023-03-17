a77a92d5d714369404273a002209b83bfaa8ca80
EzechielKiregha <ekiregha@gmail.com>
2023-03-17 15:06:02 -0700

    All digit base 10 were printed on stdo

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -We print all digit to the std output.
+ * using for loop through 0 - 10
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
