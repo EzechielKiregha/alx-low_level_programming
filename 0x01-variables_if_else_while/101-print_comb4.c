69e7a4b0be89e07d28abbd92fe7af6265c002471
EzechielKiregha <ekiregha@gmail.com>
2023-03-17 20:19:43 -0700

    101 task fix made successfully

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,63 +17,9 @@ int main(void)
 
 	for (i = 48; i <= 56; i++)
 	{
-		if (i == 48)
+		for (j = i + 1; j <= 56; j++)
 		{
-			j = 49;
-		}
-		else if (i == 49)
-		{
-			j = 50;
-		}
-		else if (i == 50)
-		{
-			j = 51;
-		}
-		else if (i == 51)
-		{
-			j = 52;
-		}
-		else if (i == 52)
-		{
-			j = 53;
-		}
-		else if (i == 53)
-		{
-			j = 54;
-		}
-		else if (i == 54)
-		{
-			j = 55;
-		}
-		else if (i == 55)
-		{
-			j = 56;
-		}
-		else
-			j = 57;
-		for (; j <= 56;)
-		{
-			if (j == 49)
-				k = 50;
-			else if (j == 50)
-				k = 51;
-			else if (j == 51)
-				k = 52;
-			else if (j == 52)
-				k = 52;
-			else if (j == 52)
-				k = 53;
-			else if (j == 53)
-				k = 54;
-			else if (j == 54)
-				k = 55;
-			else if (j == 55)
-				k = 56;
-			else if (j == 55)
-				k = 56;
-			else
-				k = 57;
-			for (; k <= 57;)
+			for (k = j + 1; k <= 57; k++)
 			{
 				putchar(i);
 				putchar(j);
@@ -87,9 +33,7 @@ int main(void)
 					putchar(',');
 					putchar(' ');
 				}
-				k++;
 			}
-			j++;
 		}
 	}
 	putchar('\n');
