64fc896ec9ce0cb035263cca168f462ea04a524e
EzechielKiregha <ekiregha@gmail.com>
2023-03-17 19:29:58 -0700

    101 Three digits combinations done

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,60 +20,76 @@ int main(void)
 		if (i == 48)
 		{
 			j = 49;
-			k = 50;
 		}
 		else if (i == 49)
 		{
 			j = 50;
-			k = 51;
 		}
 		else if (i == 50)
 		{
 			j = 51;
-			k = 52;
 		}
 		else if (i == 51)
 		{
 			j = 52;
-			k = 53;
 		}
 		else if (i == 52)
 		{
 			j = 53;
-			k = 54;
 		}
 		else if (i == 53)
 		{
 			j = 54;
-			k = 55;
 		}
 		else if (i == 54)
 		{
 			j = 55;
-			k = 56;
 		}
 		else if (i == 55)
 		{
 			j = 56;
-			k = 57;
 		}
 		else
 			j = 57;
-		for (; j <= 57;)
+		for (; j <= 56;)
 		{
+			if (j == 49)
+				k = 50;
+			else if (j == 50)
+				k = 51;
+			else if (j == 51)
+				k = 52;
+			else if (j == 52) 
+                        	k = 52;
+			else if (j == 52)
+				k = 53;
+			else if (j == 53)
+				k = 54;
+			else if (j == 54)
+				k = 55;
+			else if (j == 55)
+				k = 56;
+			else if (j == 55)
+				k = 56;
+			else
+				k = 57;
 			for (; k <= 57;)
 			{
 				putchar(i);
 				putchar(j);
 				putchar(k);
+				if (i == 55 && j == 56 && k == 57)
+				{
+					break;
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
 				k++;
-				if (i == 55)
-					continue;
-				putchar(',');
-				putchar(' ');
-			
 			}
-		j++;	
+			j++;
 		}
 	}
 	putchar('\n');
