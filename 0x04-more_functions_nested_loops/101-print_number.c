6f8b5c828d48ebcd779b1d675fa5a3f147ca1388
EzechielKiregha <ekiregha@gmail.com>
2023-03-23 19:43:44 -0700

    Task 9 and 12 twelve fixed already

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,45 +5,18 @@
  * print_number - prints an integer using _putchar function
  * @n: integer to print
  *
- * Return: void
+ * Return: no value
  */
+
 void print_number(int n)
 {
-	int num, div, digit;
-	unsigned int abs_num;
-
-	if (n == 0)
-	{
-		_putchar('0');
-		_putchar('\n');
-		return;
-	}
-
-	div = 1;
-	if (n < 0)
-	{
-		abs_num = -n;
+	if (n < 0) {
 		_putchar('-');
-	}
-	else
-	{
-		abs_num = n;
+		n = -n;
 	}
 
-	num = abs_num;
-	while (num / div >= 10)
-	{
-		div *= 10;
-	}
+	if (n / 10)
+		print_number(n / 10);
 
-	while (div != 0)
-	{
-		digit = num / div;
-		_putchar(digit + '0');
-		num %= div;
-		div /= 10;
-	}
-
-	_putchar('\n');
+	_putchar(n % 10 + '0');
 }
-
