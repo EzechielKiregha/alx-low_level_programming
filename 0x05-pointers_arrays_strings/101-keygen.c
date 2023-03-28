4569f6fc23569c1be3295e9e3a9fc33702409698
EzechielKiregha <ekiregha@gmail.com>
2023-03-28 01:59:38 -0700

    Crack me success debug

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,16 +6,14 @@
 
 int main(void)
 {
-	srand(time(NULL));
 	char password[PASSWORD_LENGTH + 1];
+	int i;
 
-	for (int i = 0; i < PASSWORD_LENGTH; i++)
+	srand(time(NULL));
+	for (i = 0; i < PASSWORD_LENGTH; i++)
 		password[i] = rand() % 94 + 33;
-
 	password[PASSWORD_LENGTH] = '\0';
-
 	printf("Random password for 101-crackme: %s\n", password);
-
-	return 0;
+	return (0);
 }
 
