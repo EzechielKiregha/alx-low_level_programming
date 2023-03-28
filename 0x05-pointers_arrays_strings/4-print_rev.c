dee38937e679cc6964fe3705cb7f76e653cc3bd7
EzechielKiregha <ekiregha@gmail.com>
2023-03-28 01:49:36 -0700

    I do not fear computer. Error removed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,9 +13,9 @@ void print_rev(char *s)
 	int len, i;
 
 	len = 0;
-	while (s[len] != 0)
+	while (s[len] != '\0')
 		len++;
-	for (i = len; i >= 0; i--)
+	for (i = len - 1; i >= 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
