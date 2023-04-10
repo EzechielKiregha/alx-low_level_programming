7448ae70dc06d2933c311ce2aa7b6bde0aba4f45
EzechielKiregha <ekiregha@gmail.com>
2023-04-10 10:39:41 -0700

    Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,52 +1,108 @@
 #include "main.h"
 
 /**
- * main - multiplies two numbers.
- * @argc: integer variable
+ * mult - multiplies and returns the peoduct of 2 numbers
+ * @n: integer variable
  * @argv: Pointer to array of strings
  *
- * Return: (0) always
+ * Return: result on succes, otherwise (1) on fail
+ */
+
+int mult(int n, char *argv[]);
+
+/**
+ * main - multiplies two numbers
+ * @argc: the number of arguments
+ * @argv: an array of pointers to the arguments
+ *
+ * Return: 0 on success, 1 on error
  */
 
 int main(int argc, char *argv[])
 {
-	int n;
-	char digit[20];
-	int a;
-	int b;
-	int i;
-	int j;
-	char err[6] = "Error";
+	int result;
+	char buffer[10];
+	char *temp = buffer;
 
-	if (argv[argc] == NULL)
+	result = mult(argc, argv);
+	if (result == -1)
+		return (1);
+	if (result == 0)
 	{
-		if (argc - 1 == 0 || argc > 3)
+		_putchar('0');
+		_putchar('\n');
+		return (0);
+	}
+	if (result < 0 && result != -1)
+	{
+		_putchar('-');
+		result = -result;
+	}
+	while (result)
+	{
+		*temp++ = result % 10 + '0';
+		result /= 10;
+	}
+	while (temp-- != buffer)
+		_putchar(*temp);
+	_putchar('\n');
+	return (0);
+}
+
+/**
+ * mult - multiplies and returns the peoduct of 2 numbers
+ * @n: integer variable
+ * @argv: Pointer to array of strings
+ *
+ * Return: result on succes, otherwise (1) on fail
+ */
+
+int mult(int n, char *argv[])
+{
+	int i, j, t, num1 = 0, num2 = 0, result;
+	char error[6] = "Error\n";
+	int echec;
+
+	if (n != 3)
+	{
+		i = 0;
+		while (error[i] != '\0')
 		{
-			for (i = 0; err[i] != '\0'; i++)
-				_putchar(err[i]);
-			_putchar('\n');
-			return (1);
+			_putchar(error[i]);
+			i++;
+			echec = - 1;
+		}	
+	}
+	for (i = 1; i < n; i++)
+	{
+		if (argv[i][0] == '-')
+		{
+			_putchar('-');
+			j = 1;
 		}
 		else
+			j = 0;
+		for (; argv[i][j]; j++)
 		{
-			a = _atoi(argv[1]);
-			b = _atoi(argv[2]);
-			if (a < 0 || b < 0)
-				_putchar('-');
-			a = _abs(a);
-			b = _abs(b);
-			n = a * b;
-			i = 0;
-			while (n != 0)
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
-				digit[i] = (n % 10) + '0';
-				n = n / 10;
-				i++;
-			}
-			for (j = i - 1; j >= 0; j--)
-				_putchar(digit[j]);
+				t = 0;
+				while (error[num1] != '\0')
+				{
+					_putchar(error[t]);
+					t++;
+					echec = - 1;
+				}
+			}	
+			if (i == 1)
+				num1 = num1 * 10 + argv[i][j] - '0';
+			else
+				num2 = num2 * 10 + argv[i][j] - '0';
 		}
-		_putchar('\n');
 	}
-	return (0);
+	if (echec < 0)
+		result = -1;
+	else 
+		result = num1 * num2;
+	return (result);
 }
