e41e986b6e2356dfdddd2d38e28923d58d8c1e86
EzechielKiregha <ekiregha@gmail.com>
2023-04-11 10:24:17 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * str_concat -  concatenates two strings.
+ * @s1: Pointer to a string
+ * @s2: Pointer to a string
+ *
+ * Return: A pointer to a string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, j;
+	char str[1000] = "";
+	char *ptr;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		str[i] = s1[i];
+	for (j = 0; s2[j] != '\0'; j++)
+		str[i + j] = s2[j];
+	str[i + j + 1] = '\0';
+	ptr = (char *)malloc(sizeof(char) * j + 1);
+	if (ptr == NULL)
+		return (NULL);
+	for (j = 0; str[j] != '\0'; j++)
+                ptr[j] = str[j];
+	return (ptr);
+}
