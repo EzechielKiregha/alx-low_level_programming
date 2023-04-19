4181309acf7b3e1133c0118c67f6d6de538d84a7
EzechielKiregha <ekiregha@gmail.com>
2023-04-18 17:43:09 -0700

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,61 @@
 #include "dog.h"
+/**
+ * _strlen - Computes the length of a string.
+ * @s: Pointer to a string of characters.
+ * Return: The length of the string.
+ */
 
+int _strlen(char *s);
+/**
+ * _strcpy -  copies the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest.
+ * @dest: Pointer to a string
+ * @src: Pointer to string
+ *
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src);
+
+/**
+ * _strlen - Computes the length of a string.
+ * @s: Pointer to a string of characters.
+ * Return: The length of the string.
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+/**
+ * _strcpy -  copies the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest.
+ * @dest: Pointer to a string
+ * @src: Pointer to string
+ *
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, len;
+
+	len = 0;
+	while (src[len] != '\0')
+		len++;
+	for (i = 0; i <= len; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
 /**
  * *new_dog - creates a new dog struct.
  * @name: pointer to string
@@ -12,13 +68,29 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dg;
+	int l1, l2;
+
+	l1 = _strlen(name);
+	l2 = _strlen(owner);
 
 	dg = malloc(sizeof(dog_t));
 	if (dg == NULL)
 		return (NULL);
-	dg->name = name;
+	dg->name = malloc(sizeof(char) * (l1 + 1));
+	if (dg->name == NULL)
+	{
+		free(dg->name);
+		free(dg);
+		return (NULL);
+	}
+	dg->owner = malloc(sizeof(char) * (l2 + 1));
+	if (dg->owner == NULL)
+	{
+		free(dg->owner);
+		return (NULL);
+	}
 	dg->age = age;
-	dg->owner = owner;
-
+	_strcpy(dg->name, name);
+	_strcpy(dg->owner, owner);
 	return (dg);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
 #include <stdlib.h>
 /**
  * struct dog - holds informations name, age, owner
@@ -18,6 +19,8 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 void free_dog(dog_t *d);
 
 #endif
