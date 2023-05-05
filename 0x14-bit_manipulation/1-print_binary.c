f318457782eea753b5efb1a68db37dee32cd907a
EzechielKiregha <ekiregha@gmail.com>
2023-05-04 18:02:09 -0700

    Converting a number to its binary value

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *  print_binary - prints the binary valu representation of a number
+ *  @n: the decimal number that get converted
+ *
+ *  Return: void
+ *
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int lg;
+	int c, zero = 0;
+
+	c = 63;
+	while (c >= 0)
+	{
+		lg = n >> c;
+		if (lg & 1)
+		{
+			_putchar('1');
+			zero++;
+		}
+		else if (zero)
+			_putchar('0');
+		c--;
+	}
+	if (!zero)
+		_putchar('0');
+	return;
+}
