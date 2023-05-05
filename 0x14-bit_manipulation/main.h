49e99c4be028fd497a051b3cd58139979489179a
EzechielKiregha <ekiregha@gmail.com>
2023-05-04 18:03:24 -0700

    Converting a number to its binary value

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -8,6 +10,6 @@ int get_bit(unsigned long int n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
-
+int _putchar(int c);
 
 #endif
