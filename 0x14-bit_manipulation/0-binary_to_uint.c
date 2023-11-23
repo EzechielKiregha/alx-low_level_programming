b635c69f0981a263d7c4980aaab0f883779d2b79
EzechielKiregha <ekiregha@gmail.com>
2023-11-23 17:50:50 +0200

    Bitwise - convert binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,7 +9,7 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int dec = 0;
+	unsigned int decimal = 0;
 	int i;
 
 	if (!b)
@@ -19,8 +19,8 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		dec = 2 * dec + (b[i] - '0');
+		decimal = 2 * decimal + (b[i] - '0');
 	}
 
-	return (dec);
+	return (decimal);
 }
