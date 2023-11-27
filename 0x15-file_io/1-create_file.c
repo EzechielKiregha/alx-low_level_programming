09ad33d8ee69eee77badec58a69670e0775e853c
EzechielKiregha <ekiregha@gmail.com>
2023-11-27 23:22:56 +0200

    Append The Text To the File  with success

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,6 +23,6 @@ int create_file(const char *filename, char *text_content)
 	w = write(fd, text_content, lenght);
 	if (fd == -1 || w == -1)
 		return (-1);
-	close(fd);
+	close(fd);O_
 	return (1);
 }
